66e22ab550a76071aadc1b716d5ef09d94772c3b
chash-code <panchalchash@gmail.com>
2026-03-15 15:12:10 +0530

    backend server entry point

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+const accountRoutes = require('./routes/accountRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/account', accountRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Account Management API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
